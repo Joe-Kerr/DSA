4c0d1e641d4aced0c33bf1c8e1595f7f9310647f
Joe-Kerr <joe-kerr@gmx.net>
2025-02-16 17:43:30 +0100

    add BFS find path

---
+++ src/algorithms/BreadthFirstSearch.ts
@@ -0,0 +1,71 @@
+export interface IGetGraphNeighbours {
+    addNeighboursToRef(node : number, neighbourArray : number[]) : void
+}
+
+//#todo performance tests
+export function breadthFirstSearchFind(start : number, find : number, graph : IGetGraphNeighbours) {
+    const front : number[] = [start];
+    /**@alias visited */
+    const links = {};
+    const path : number[] = [];
+
+    let curNode : number|undefined = 0;
+    let neighNode = 0;
+    let safety = 10_000;
+    while(true && safety-- >= 0) {
+        //Dequeue
+        curNode = front.shift();
+
+        if(curNode === undefined || curNode === find) {         
+            break;
+        }
+
+        graph.addNeighboursToRef(curNode, front);
+
+        for(let i=0; i<front.length; i++) {
+            neighNode = front[i];
+            
+            //  neighNode == unvisited
+            if(links[neighNode] === undefined) {
+                front.push(neighNode);
+                //Need to backtrack!
+                //links[curNode] = neighNode;
+                links[neighNode] = curNode;
+            }
+        }
+    }
+
+    if(safety <= 0) {
+        console.error("INF LOOP DETECTED");
+        curNode = undefined;
+    }
+
+    safety = 10_000;
+    if(curNode === find) {
+        let step : number|undefined = find;
+        while(step !== undefined && safety-- >= 0) {
+            //#todo is this possible? Need unit test to trigger this branch
+            if(path.includes(step) === true) {
+                path.length = 0;
+                console.error("BFS backtracked into cyclical path");
+                break;
+            }
+
+            path.unshift(step);        
+            step = links[step];
+
+            if(step === start) {
+                path.unshift(step);     
+                break;
+            }
+        }        
+    }
+
+    if(safety <= 0) {
+        console.error("INF LOOP DETECTED 2");
+        path.length = 0;
+    }
+    
+
+    return path;
+}
--- tests/unit/searchAlgorithms.js
+++ tests/unit/searchAlgorithms.js
@@ -1,5 +1,6 @@
 import assert from "node:assert";
 import {binarySearch, stats} from "../../js/src/algorithms/BinarySearch.js";
+import {breadthFirstSearchFind} from "../../js/src/algorithms/BreadthFirstSearch.js";
 
 suite("Search algorithms");
 
@@ -22,4 +23,83 @@ test("binarySearch", ()=>{
 	i = binarySearch(arr, 6);	
 	assert.equal(i, 5);
 	assert.equal(stats.iterations, 3);		
+});
+
+//#todo measure performance (I needed this quickly)
+//#todo move into own class
+test("breadthFirstSearchFind simple path search", ()=>{
+	const graph = {
+		1: [2, 3],
+			2: [4, 5, 6],
+				6: [9, 10],
+					10: [11],
+			3: [7, 8],
+				7: [12, 13]
+	};
+	
+	function addNeighbours(node, out) {
+		if(graph[node]) {
+			out.push(...graph[node]);
+		}
+	}
+	
+	let simple;
+
+	simple = breadthFirstSearchFind(1, 11, {addNeighboursToRef: addNeighbours});
+	
+	assert.deepEqual(simple, [1,2,6,10,11]);
+});
+
+test("breadthFirstSearchFind no path possible", ()=>{
+	const graph = {
+		1: [2, 3],
+			2: [4, 5, 6],
+				6: [9, 10],
+					10: [11],
+			3: [7, 8],
+				7: [12, 13]
+	};
+	
+	function addNeighbours(node, out) {
+		if(graph[node]) {
+			out.push(...graph[node]);
+		}
+	}
+	
+	let simple;
+
+	simple = breadthFirstSearchFind(2, 13, {addNeighboursToRef: addNeighbours});
+	assert.deepEqual(simple, [], "No connection");
+	
+	simple = breadthFirstSearchFind(23, 13, {addNeighboursToRef: addNeighbours});
+	assert.deepEqual(simple, [], "Invalid start");		
+	
+	simple = breadthFirstSearchFind(1, 23, {addNeighboursToRef: addNeighbours});
+	assert.deepEqual(simple, [], "Invalid target");		
+	
+	//#todo is a 1-step path a path?
+	simple = breadthFirstSearchFind(3, 3, {addNeighboursToRef: addNeighbours});
+	assert.deepEqual(simple, [3], "start == find");	
+});
+
+
+test("breadthFirstSearchFind cyclical path", ()=>{
+	const graph = {
+		1: [2],
+			2: [1, 3],
+				3: [1, 4],
+					4: [5]
+	};
+	
+	function addNeighbours(node, out) {
+		if(graph[node]) {
+			out.push(...graph[node]);
+		}
+	}
+	
+	let simple;
+
+	simple = breadthFirstSearchFind(1, 5, {addNeighboursToRef: addNeighbours});
+	
+	assert.deepEqual(simple, [1,2,3,4,5]);
 });
